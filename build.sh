f5f1ad91e9fb37361d99731b28a7d6d862f82d63
TGH131 <107738130+TGH131@users.noreply.github.com>
2022-06-25 00:20:10 +0530

    be quiet

--- build.sh
+++ build.sh
@@ -59,7 +59,7 @@ java -jar "revanced-cli.jar" --experimental -a "unp.apk" -b "revanced-patches.ja
 
 zip -d pat.apk "lib/armeabi-v7a/*" "lib/x86_64/*" "lib/x86/*" || exit 1
 
-$ANDROID_SDK_ROOT/build-tools/33.0.0/zipalign -p -f -v 4 pat.apk alpat.apk || exit 1
+$ANDROID_SDK_ROOT/build-tools/33.0.0/zipalign -p -f 4 pat.apk alpat.apk || exit 1
 
 zip -q -9 zippy.zip alpat.apk
 
